742485b9201b10c414927493d05b6fb4c0fce258
Bethel Eyo <betheleyo@gmail.com>
2024-12-08 22:44:01 +0000

    Implement Persistent storage of todos

--- app/contexts/TodoProvider.tsx
+++ app/contexts/TodoProvider.tsx
@@ -1,22 +1,47 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { TodoContext, TodoProps } from "./TodoContext";
+import * as SecureStore from "expo-secure-store";
 
 export const TodoProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
   const [todos, setTodos] = useState<TodoProps[]>([]);
+  const STORE_ACCESS_KEY = "todos";
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const todoListString = await SecureStore.getItemAsync(STORE_ACCESS_KEY);
+        const todos: TodoProps[] = todoListString ? JSON.parse(todoListString) : [];
+        setTodos(todos);
+      } catch (error) {
+        console.debug(error);
+      }
+    };
+
+    getTodos();
+  }, []);
+
+  const storeTodosUpdate = async (todos: TodoProps[]) => {
+    try {
+      await SecureStore.setItemAsync(STORE_ACCESS_KEY, JSON.stringify(todos));
+      setTodos(todos);
+    } catch (error) {
+      console.debug(error);
+    }
+  };
 
   const addTodo = (todo: TodoProps) => {
     const newTodos = [...todos, todo];
-    setTodos(newTodos);
+    storeTodosUpdate(newTodos);
   };
 
-  const editTodo = (todo: TodoProps) => {
+  const editTodo = async (todo: TodoProps) => {
     const updatedTodos = todos.map((prevTodo) => (todo.id === prevTodo.id ? todo : prevTodo));
-    setTodos(updatedTodos);
+    storeTodosUpdate(updatedTodos);
   };
 
   const deleteTodo = (id: number) => {
     const remainingTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(remainingTodos);
+    storeTodosUpdate(remainingTodos);
   };
 
   return (
