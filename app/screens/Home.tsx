3be83bee4f8a0898f2b262995d36291cd290c38e
Bethel Eyo <betheleyo@gmail.com>
2024-12-09 13:41:27 +0000

    implement useHomeLogic hook
    refactoring the home screen to remove the logic from the view

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,72 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
   TextInput,
-  Animated,
-  Dimensions,
+  TouchableOpacity,
+  View
 } from "react-native";
-import TodoItem from "../components/TodoItem";
 import "react-native-get-random-values";
-import { v4 as uuidv4 } from "uuid";
-import { TodoProps } from "../contexts/TodoContext";
-import useTodoContext from "../contexts/useTodoContext";
 import AnimatedModal from "../components/AnimatedModal";
+import TodoItem from "../components/TodoItem";
+import useHomeLogic from "./useHomeLogic";
 
 const Home = () => {
-  const { todos, addTodo, editTodo, completedTodos, uncompletedTodos } = useTodoContext();
-  const [title, setTitle] = useState<string | undefined>(undefined);
-  const [selectedOption, setSelectedOption] = useState<string>("All");
-  const options = ["All", "Completed", "Uncompleted"];
-  const [modalVisible, setModalVisible] = useState(false);
-  const { height } = Dimensions.get("window");
-  const [slideAnim] = useState(new Animated.Value(height)); // Initial position off-screen
-
-  const openModal = () => {
-    setModalVisible(true);
-    Animated.timing(slideAnim, {
-      toValue: 0, // Slide up to show modal
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const closeModal = () => {
-    Animated.timing(slideAnim, {
-      toValue: height, // Slide down to hide modal
-      duration: 300,
-      useNativeDriver: true,
-    }).start(() => setModalVisible(false));
-  };
-
-  const add = () => {
-    if (title) {
-      const todo: TodoProps = {
-        title,
-        completed: false,
-        id: uuidv4(),
-      };
-      addTodo(todo);
-      setTitle(undefined);
-    }
-  };
-
-  const markAsCompleted = (todo: TodoProps) => {
-    const { title, completed, id } = todo;
-    const updatedTodo: TodoProps = {
-      title,
-      completed: !completed,
-      id,
-    };
-    editTodo(updatedTodo);
-    setSelectedOption("All");
-    openModal();
-  };
+  const {
+    todos,
+    completedTodos,
+    uncompletedTodos,
+    selectedOption,
+    modalVisible,
+    options,
+    markAsCompleted,
+    add,
+    closeModal,
+    setSelectedOption,
+    title,
+    setTitle,
+    slideAnim
+  } = useHomeLogic();
 
   return (
     <View style={styles.container}>
